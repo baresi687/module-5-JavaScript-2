486dcd029ec1071a565634ca89edcfa52ec8fb06
hreinn <hreinngy@gmail.com>
2022-10-06 13:12:58 +0200

    Linear search

---
+++ day-1-start/2-intro-to-linear-search/js/script.js
@@ -0,0 +1,10 @@
+// linear search
+
+const array = [5, 6, 3, 8, 3]
+
+function linearSearch(arr, value) {
+  return arr.indexOf(value)
+}
+
+const result = linearSearch(array, 8)
+console.log(result);
